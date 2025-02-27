cce6c88dbec162d777accd2959d4fef5141b298f
Maksims Turs <maksims07turs@gmail.com>
2025-02-27 18:18:00 +0100

    Performance boost, added function to creating string headers.

--- CHTTPHeader.h
+++ CHTTPHeader.h
@@ -2,6 +2,8 @@
 
 /*=======================================================================*/
 
+#include <stdio.h>
+
 #include "./include/CString.h"
 
 /*=======================================================================*/
@@ -10,7 +12,6 @@ typedef unsigned char      CHHDR_Ret_Code;
 typedef unsigned char      CHHDR_Bool;
 typedef unsigned char      CHHDR_UChar;
 typedef char               CHHDR_Char;
-typedef char*              CHHDR_Char_Ptr;
 
 typedef void*              CHHDR_Any;
 typedef void               CHHDR_None;
@@ -19,8 +20,8 @@ typedef unsigned int       CHHDR_UInt;
 
 typedef unsigned short     CHHDR_UShort;
 
-typedef unsigned long long CHHDR_Hash;
 typedef unsigned long long CHHDR_ULLong;
+typedef long long          CHHDR_LLong;
 
 typedef float              CHHDR_Float;
 
@@ -39,36 +40,46 @@ typedef float              CHHDR_Float;
 #define CHHDR_TRUE                        (CHHDR_UChar)1
 #define CHHDR_FALSE                       (CHHDR_UChar)0
 // Errors/Succes constants.
-#define CHHDR_ERROR_ILLEGAL_HTTP_TYPE     (CHHDR_UChar)3
-#define CHHDR_ERROR_ILLEGAL_METHOD        (CHHDR_UChar)5
-#define CHHDR_ERROR_ILLEGAL_STATE         (CHHDR_UChar)8
-#define CHHDR_SUCCESS                     (CHHDR_UChar)12
-// URL Parser states.
+#define CHHDR_ERROR_INVALID_HTTP_TYPE     (CHHDR_UChar)0
+#define CHHDR_ERROR_INVALID_METHOD        (CHHDR_UChar)1
+#define CHHDR_ERROR_INVALID_STATE         (CHHDR_UChar)2
+#define CHHDR_ERROR_INVALID_PTR           (CHHDR_UChar)3
+#define CHHDR_ERROR_BUFFER_OVERFLOW       (CHHDR_UChar)4
+#define CHHDR_SUCCESS                     (CHHDR_UChar)5
+// Parser states.
 #define CHHDR_STATE_START                 (CHHDR_UChar)0
 #define CHHDR_STATE_KEY                   (CHHDR_UChar)1
 #define CHHDR_STATE_VALUE                 (CHHDR_UChar)2
 #define CHHDR_STATE_BETWEEN_KEY_AND_VALUE (CHHDR_UChar)3
+#define CHHDR_STATE_END                   (CHHDR_UChar)4
 
-#define CHHDR_SAFE_CALL(error_code, success_code) do { \
-	if(error_code != success_code) return error_code;    \
+#define CHHDR_RAW_HEADER_APPEND           (CHHDR_UChar)1
+#define CHHDR_RAW_HEADER_RESET            (CHHDR_UChar)2
+
+#define CHHDR_FAIL_IF(condition, error_code) do { \
+	if(condition) return error_code;                \
 } while(0);
 #define CHHDR_PEAK_TOKEN(str, offset) *(str + offset)
+#define CHHDR_LIKELY(condition)       __builtin_expect(!!(condition), CHHDR_TRUE)
+#define CHHDR_UNLIKELY(condition)     __builtin_expect(!!(condition), CHHDR_FALSE)
 
 /*=======================================================================*/
 
-typedef void (*CHHDR_on_head)(CSTR_Slice slice_1, CSTR_Slice slice_2, CSTR_Slice slice_3);
-typedef void (*CHHDR_on_key_value)(CSTR_Slice key, CSTR_Slice value); 
+typedef void (*CHHDR_on_head)(CHHDR_Any user_data, CSTR_Slice slice_1, CSTR_Slice slice_2, CSTR_Slice slice_3);
+typedef void (*CHHDR_on_key_value)(CHHDR_Any user_data, CSTR_Slice key, CSTR_Slice value);
 
 typedef struct {
-	const CHHDR_Char_Ptr string;
-	const CHHDR_UChar type;
-	const CHHDR_UShort length;
 	const CHHDR_on_key_value on_key_value;
 	const CHHDR_on_head on_head;
+	const CHHDR_UChar type;
+	CHHDR_Any user_data;
+	CHHDR_Char* string;
+	CHHDR_UShort length;
 } CHHDR_Header;
 
 /*=======================================================================*/
 
-CHHDR_Ret_Code chhdr_parse(CHHDR_Header const header);
+CHHDR_Ret_Code chhdr_parse(CHHDR_Header header);
+CHHDR_Ret_Code chhdr_append(CHHDR_Char* buff, CHHDR_UShort buff_size, const CHHDR_Char* key, CHHDR_UShort key_len, CHHDR_Char* value, CHHDR_UShort value_len, CHHDR_UChar flag);
 
 /*=======================================================================*/
